ea1ade94583bfba00b897cadc8a070090ffc7137
nelsonCHernandez <45251277+nelsonCHernandez@users.noreply.github.com>
2019-03-26 00:37:44 -0400

    Update send.php

--- send.php
+++ send.php
@@ -1,16 +1,16 @@
 <?php
 $name = $_POST['name'];
-$useremail = $_POST['email'];
+$userEmail = $_POST['email'];
 $msg = $_POST['msg'];
 
 $emailFrom = 'mercuriss365@gmail.com';
 $emailSubject = 'New Form Submission';
-$emailBody = "You have received a new message from the user $name.\n". 
-"Here is the message:\n $message";
-// mail(to,subject,message,headers)
+$emailBody = "You have received a new message from the user $name.\n".
+"Here is the message:\n $msg\n".
+"\n Reply-To: $userEmail\n";
+// mail
 $to = "nelsoncamposh05@gmail.com";
-$headers = "From: $email_From \r\n";
-$headers .= "Reply-To: $useremail \r\n";
+$headers = "From: $emailFrom \r\n";
+"Reply-To: $userEmail \r\n".
 mail($to, $emailSubject, $emailBody, $headers);
-
 ?>
